def1282823845e5a6147f679ea0b1df02ab0b82c
Diego <iwujigoodluck5@gmail.com>
2025-09-07 15:53:35 +0000

    MCV architecture done

---
+++ src/appController.ts
@@ -0,0 +1,13 @@
+import { renderWeather } from "./views/weatherView.js";
+import { getWeatherData } from "./weatherModel.js";
+
+function greet(name: string): string {
+  return `Hello, ${name}! Your TypeScript is working.`;
+}
+
+const data = getWeatherData();
+renderWeather(data);
+
+console.log("Controller ran successfully.");
+
+console.log(greet("Weather Hacker"));
